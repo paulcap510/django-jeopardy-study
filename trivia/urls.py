b7ae8c025e07b3800eb0bae3e90dc18402393583
Paul <paulcapob@gmail.com>
2026-08-04 14:45:44 -0400

    Disable unprotected Jinja write routes

--- trivia/urls.py
+++ trivia/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
         name="api_category_entries",
     ),
     path("api/search/", views.api_search_results, name="api_search_results"),
-    # path('api/entries/generate/', views.api_generate_entry, name='api_generate_entry'),
+    path("api/entries/generate/", views.api_generate_entry, name="api_generate_entry"),
     path(
         "api/entries/<int:entry_id>/", views.api_entry_detail, name="api_entry_detail"
     ),
